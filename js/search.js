47fa2a7ead2252d337f6ee8fa57d5277016b34f1
Peter Connell <peterconnellit@gmail.com>
2022-10-25 00:07:33 +0100

    javaScript error

--- js/search.js
+++ js/search.js
@@ -13,7 +13,21 @@ function retrieve(e){
 
     let url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}93fa9c4c03804d35b9a4178db6eb4808`
 
-    fetch(url).then((res)=>)
+    fetch(url).then((res)=>{
+        return res.json()
+    }).then((data)=>{
+        console.log(data)
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        a.setAttribute('href', article.url );
+        a.setAttribute('target', '_blank');
+        a.textContent = article.title;
+        li.appendChild(a);
+        newsList.appendChild(li);
+
+
+    })
+    }
 
 
 }

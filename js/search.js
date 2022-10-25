dd7bb10f78fb95cb0f235c7444c155c34564a685
Peter Connell <peterconnellit@gmail.com>
2022-10-25 12:39:07 +0100

    working search form

--- js/search.js
+++ js/search.js
@@ -1,33 +1,66 @@
-const searchFrom = document.querySelector('.search');
-const input = document.querySelector('.input');
-const newsList = document.querySelector('news-list');
+window.onload=function(){
+    
+            //Declare selectors to retrieve user input and from form and append results to news list
+            const searchFrom = document.querySelector('.search-form');
+            const input = document.querySelector('.input');
+            const newsList = document.querySelector('.news-list');
+    
+            //listen for submit which triggers retrieve function
+            searchFrom.addEventListener('submit', retrieve)        
+    
+            //Initialise function, listens for event
+            function retrieve(e) {
+    
+                //Alert user to input search criteria
+                if (input.value ==''){
+                    alert('Input field is empty')
+                    return
+                }
+    
+                //Clear and allow multiple searches
+                newsList.innerHTML=''
+    
+                e.preventDefault()
+    
+                /*Would recommend storing in an environmental variable from user.
+                For demonstration purposes*/
+                const apiKey = '93fa9c4c03804d35b9a4178db6eb4808'
+    
+                //Convert user input from form to topic variable for use within url template literal query
+                let topic = input.value;
+    
+                //Fetch data from API
+                let url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`
+    
+                //Built in fetch API
+                fetch(url).then((res) => {
+                    //Return response in json format
+                    return res.json()
+                }).then((data) => {
+                    //Get data and show response on console
+                    console.log(data)
+    
+                    //Callback function to create new list element and anchor tag
+                    data.articles.forEach(article => {
+                        let li = document.createElement('li');
+                        let a = document.createElement('a');
+                        //Set list a tag attribute to URL
+                        a.setAttribute('href', article.url);
+                        //Prevent page from directing user away
+                        a.setAttribute('target', '_blank');
+                        //Sets link as article title
+                        a.textContent = article.title;
+                        //Append anchor tag to list element
+                        li.appendChild(a);
+                        //Append list to news-list
+                        newsList.appendChild(li);
+                    })
+    
+                }).catch((error)=>{
+                    console.log(error)
+                })
+            }
+  }
+        
+        
 
-searchFrom.addEventListener('submit', retrieve)
-
-function retrieve(e){
-
-    e.preventDefault()
-
-    const apiKey = '93fa9c4c03804d35b9a4178db6eb4808'
-    let topic = input.Value;
-
-    let url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}93fa9c4c03804d35b9a4178db6eb4808`
-
-    fetch(url).then((res)=>{
-        return res.json()
-    }).then((data)=>{
-        console.log(data)
-        let li = document.createElement('li');
-        let a = document.createElement('a');
-        a.setAttribute('href', article.url );
-        a.setAttribute('target', '_blank');
-        a.textContent = article.title;
-        li.appendChild(a);
-        newsList.appendChild(li);
-
-
-    })
-    }
-
-
-}
